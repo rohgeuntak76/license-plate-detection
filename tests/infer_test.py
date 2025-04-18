e8e6a5c8ff3dc6db52c2de3365d114c43ac8d334
rohgeuntak76 <guen-tak.roh@hpe.com>
2025-04-18 17:12:56 +0900

    Sample tracking code against video

---
+++ tests/infer_test.py
@@ -0,0 +1,36 @@
+import cv2
+
+from ultralytics import YOLO
+
+# Load the YOLO11 model
+model = YOLO("yolo11n.pt")
+
+# Open the video file
+video_path = "../inputs/sample.mp4"
+cap = cv2.VideoCapture(video_path)
+
+# Loop through the video frames
+while cap.isOpened():
+    # Read a frame from the video
+    success, frame = cap.read()
+
+    if success:
+        # Run YOLO11 tracking on the frame, persisting tracks between frames
+        results = model.track(frame, persist=True)
+        # results = model.track(frame)
+
+        # Visualize the results on the frame
+        annotated_frame = results[0].plot()
+
+        # Display the annotated frame
+        cv2.imshow("YOLO11 Tracking", annotated_frame)
+
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        # Break the loop if the end of the video is reached
+        break
+
+cap.release()
+cv2.destroyAllWindows()
