59f4a08c6a70d7262cae8076e77c14cd7120db46
rohgeuntak76 <guen-tak.roh@hpe.com>
2025-04-25 13:37:43 +0900

    rgb , bgr channel test

--- tests/simple/simple_infer.py
+++ tests/simple/simple_infer.py
@@ -2,4 +2,11 @@ from ultralytics import YOLO
 
 # Load the model and run the tracker with a custom configuration file
 model = YOLO("yolo11n.pt")
-results = model.track(source="https://youtu.be/LNwODJXcvt4", show=True)
+# results = model.track(source="https://youtu.be/LNwODJXcvt4", show=True)
+results = model(source="./car_image.jpg")
+
+bgr = results[0].plot()
+print(bgr.shape)
+exit()
+print(type(results))
+print(results[0].plot().shape())
