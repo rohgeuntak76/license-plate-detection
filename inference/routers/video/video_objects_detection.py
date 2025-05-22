44fa40654772b651eb7abf8e9e76eba69ee60383
rohgeuntak76 <guen-tak.roh@hpe.com>
2025-05-22 13:58:10 +0900

    remove upload api to utils.

--- inference/routers/video/video_objects_detection.py
+++ inference/routers/video/video_objects_detection.py
@@ -6,71 +6,22 @@ from websockets.exceptions import InvalidState
 import uuid
 import asyncio
 
-import numpy as np
 import cv2 as cv
-from PIL import Image
-import io
 import uuid
 
-import easyocr
-from ultralytics import YOLO
-
 from utils.license_format import write_csv
-from utils.detector import car_detect_bytes,license_detect_bytes, crop_car_license_then_read
-# from utils.detector import read_license_plate
-# from utils.detector import car_tracker_numpy,license_detect_numpy
+from utils.detector import car_detect_bytes,license_detect_bytes, reset_tracker
 
-# car_detector = YOLO("./models/yolo11s.pt")
-# license_detector = YOLO("./models/yolo11s_20epochs_best.pt")
-# plate_reader = easyocr.Reader(['en'],gpu=False)
 
 router = APIRouter(
     prefix="/api/video",
 )   
 
-@router.post("/plate_number/crop/detect/info",tags=["Plate Number Detection"])
-async def video_plate_number_detect(file: UploadFile,car_conf: float = Form(0.25),license_conf: float = Form(0.25)):
-    temp_path = f"temp_{uuid.uuid4()}.mp4"
-    with open(temp_path,"wb") as buffer:
-        buffer.write(await file.read())
-    
-    video = cv.VideoCapture(temp_path)
-    fps = video.get(cv.CAP_PROP_FPS)
-    print(fps)
-    frame_nmr = -1
-    
-    results = {}
-    try:
-        while video.isOpened():
-            frame_nmr += 1
-            success, frame = video.read()
-            # print(frame_nmr)
-            # if success:
-            if success and frame_nmr < 10:
-                results[frame_nmr] = crop_car_license_then_read(frame,car_conf,license_conf)
-            else:
-                break
-    finally:
-        write_csv(results, './temp_video.csv')
-        video.release()
-        if os.path.exists(temp_path):
-            os.remove(temp_path)
-                        
-
-
-@router.post("/upload",tags=['Utils'])
-async def upload_video(file: UploadFile):
-    # Save uploaded video temporarily
-    temp_path = f"temp_{uuid.uuid4()}.mp4"
-    with open(temp_path, "wb") as buffer:
-        buffer.write(await file.read())
-    
-    # Return session ID for WebSocket connection
-    session_id = str(uuid.uuid4())
-    return {"session_id": session_id, "video_path": temp_path}
-
 @router.websocket("/ws/car/{session_id}")
 async def process_video_ws_car(websocket: WebSocket, session_id: str):
+    '''
+    Does not Use Tracker
+    '''
     await websocket.accept()
     
     # Get video path from session data (simplified here)
@@ -91,7 +42,7 @@ async def process_video_ws_car(websocket: WebSocket, session_id: str):
             # if not ret :
                 break
 
-            return_bytes = car_detect_bytes(frame,conf,frame=True)
+            return_bytes = car_detect_bytes(frame,conf)
             # # Send bytes 
             if websocket.application_state != websockets.WebSocketState.DISCONNECTED:
                 await websocket.send_bytes(return_bytes)
@@ -103,6 +54,8 @@ async def process_video_ws_car(websocket: WebSocket, session_id: str):
     finally:
         print(websocket.client_state)
         print(websocket.application_state)
+        # if reset_tracker():
+        #     print('tracker reset done!')
         if websocket.application_state != websockets.WebSocketState.DISCONNECTED:
             await websocket.close(reason="Normal closure")
         else:
@@ -114,6 +67,9 @@ async def process_video_ws_car(websocket: WebSocket, session_id: str):
 
 @router.websocket("/ws/license_plate/{session_id}")
 async def process_video_ws_license_plate(websocket: WebSocket, session_id: str):
+    '''
+    Does not Use Tracker
+    '''
     await websocket.accept()
     
     # Get video path from session data (simplified here)
@@ -148,6 +104,8 @@ async def process_video_ws_license_plate(websocket: WebSocket, session_id: str):
     finally:
         print(websocket.client_state)
         print(websocket.application_state)
+        # if reset_tracker():
+        #     print('tracker reset done!')
         if websocket.application_state != websockets.WebSocketState.DISCONNECTED:
             await websocket.close(reason="Normal closure")
         else:
