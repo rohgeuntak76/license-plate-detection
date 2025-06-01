cb17418ed7867ae2107b68fefa323064fa6c2a3a
rohgeuntak76 <guen-tak.roh@hpe.com>
2025-06-01 21:44:23 +0900

    Refactor video websocket apis

--- inference/routers/video/video_plate_number_detection.py
+++ inference/routers/video/video_plate_number_detection.py
@@ -36,26 +36,28 @@ async def process_video_ws_license_number(websocket: WebSocket, session_id: str)
         while cap.isOpened():
             ret, frame = cap.read()
             ### limit 10 frame for test purpose
-            if not ret or frame_num > 10:
-            # if not ret :
+            # if not ret or frame_num > 20:
+            if not ret :
                 break
             
             frame_num += 1
             # Process frame - detect license plates
             detection_results = crop_vehicle_license_then_read(input_image=frame,vehicle_conf=vehicle_conf,license_conf=license_conf,frame_number=frame_num)
-            # # Convert frame to bytes and send
+            # Convert frame to bytes and send
             if websocket.application_state != websockets.WebSocketState.DISCONNECTED:
                 await websocket.send_json(detection_results)
     
-            # Control processing rate to not overwhelm the connection
+            # # Control processing rate to not overwhelm the connection ( without this sleep, dataframe print and interrupt inference won't work)
             await asyncio.sleep(1/fps)
+
     except (InvalidState,WebSocketDisconnect) as e:
         print(f"{e}")
     finally:
-        print(websocket.client_state)
-        print(websocket.application_state)
+        print(f"websocket client state : {websocket.client_state}. websocket application state : {websocket.application_state}")
+        
         if reset_tracker():
             print('tracker reset done!')
+        
         if websocket.application_state != websockets.WebSocketState.DISCONNECTED:
             await websocket.close(reason="Normal closure")
         else:
--- inference/routers/video/video_vehicle_detection.py
+++ inference/routers/video/video_vehicle_detection.py
@@ -34,24 +34,22 @@ async def process_video_ws_vehicle(websocket: WebSocket, session_id: str):
             frame_num += 1
             ret, frame = cap.read()
             ### limit 10 frame for test purpose
-            if not ret or frame_num > 10:
-            # if not ret :
+            # if not ret or frame_num > 10:
+            if not ret :
                 break
 
             return_bytes = vehicle_detect_bytes(frame,conf)
-            # # Send bytes 
+            # Send bytes 
             if websocket.application_state != websockets.WebSocketState.DISCONNECTED:
                 await websocket.send_bytes(return_bytes)
     
-            # Control processing rate to not overwhelm the connection
-            await asyncio.sleep(1/fps)
+            # # Control processing rate to not overwhelm the connection
+            # await asyncio.sleep(1/fps)
     except (InvalidState,WebSocketDisconnect) as e:
         print(f"{e}")
     finally:
-        print(websocket.client_state)
-        print(websocket.application_state)
-        # if reset_tracker():
-        #     print('tracker reset done!')
+        print(f"websocket client state : {websocket.client_state}. websocket application state : {websocket.application_state}")
+        
         if websocket.application_state != websockets.WebSocketState.DISCONNECTED:
             await websocket.close(reason="Normal closure")
         else:
@@ -82,26 +80,24 @@ async def process_video_ws_license_plate(websocket: WebSocket, session_id: str):
         while cap.isOpened():
             ret, frame = cap.read()
             ### limit 10 frame for test purpose
-            if not ret or frame_num > 10:
-            # if not ret :
+            # if not ret or frame_num > 10:
+            if not ret :
                 break
             
             frame_num += 1
             # Process frame - detect license plates
             return_bytes = license_detect_bytes(frame,conf)
-            # # Convert frame to bytes and send
+            # Convert frame to bytes and send
             if websocket.application_state != websockets.WebSocketState.DISCONNECTED:
                 await websocket.send_bytes(return_bytes)
     
-            # Control processing rate to not overwhelm the connection
-            await asyncio.sleep(1/fps)
+            # # Control processing rate to not overwhelm the connection
+            # await asyncio.sleep(1/fps)
     except (InvalidState,WebSocketDisconnect) as e:
         print(f"{e}")
     finally:
-        print(websocket.client_state)
-        print(websocket.application_state)
-        # if reset_tracker():
-        #     print('tracker reset done!')
+        print(f"websocket client state : {websocket.client_state}. websocket application state : {websocket.application_state}")
+
         if websocket.application_state != websockets.WebSocketState.DISCONNECTED:
             await websocket.close(reason="Normal closure")
         else:
