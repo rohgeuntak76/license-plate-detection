256ddad6426a8de4f46d3f5a1368bd8a2abab317
rohgeuntak76 <guen-tak.roh@hpe.com>
2025-06-01 17:43:03 +0900

    Refactor the model info api

--- inference/routers/utils/model_info.py
+++ inference/routers/utils/model_info.py
@@ -1,23 +1,41 @@
 from fastapi import APIRouter
+from pydantic import BaseModel,Field
 import yaml
+
+class ModelInfo(BaseModel):
+    VEHICLE_MODEL_NAME : str = Field(..., description="Vehicle detection Model")
+    LICENSE_MODEL_NAME : str = Field(..., description="License Plate detection Model")
+    class Config:
+        json_schema_extra = {
+            "examples": [
+                {
+                    "VEHICLE_MODEL_NAME": "./models/yolo11s.pt",
+                    "LICENSE_MODEL_NAME": "./models/yolo11s_20epochs_best.pt"
+                },
+                {
+                    "VEHICLE_MODEL_NAME": "https://[kserve isvc url]/vehicle_detector",
+                    "LICENSE_MODEL_NAME": "https://[kserve isvc url]/license_detector"
+                },
+            ]
+        }
 
 router = APIRouter(
     prefix="/api/utils",
 )
 
-@router.get("/models/info", tags=["Utils"])
+@router.get(
+    "/models/info",
+    tags=["Utils"],
+    response_model=ModelInfo,
+    summary="Return Model info",
+    description="Return Model info from config.yaml"
+)
 def return_model_info():
-    """
-    Return Model info from config.yaml
-        
-    Returns:
-        Dict: { "VEHICLE_MODEL_NAME" : VEHICLE_MODEL_NAME, "LICENSE_MODEL_NAME": LICENSE_MODEL_NAME}
-    """
-    with open("/Users/geuntakroh/workspace/github/license-plate-detection/inference/config.yaml", "r") as f:
+    with open("./config.yaml", "r") as f:
         config = yaml.safe_load(f)
 
-    # TOKEN = config["detectors"]["server_token"]
     MODEL_ENDPOINT = config["detectors"]["server_url"]
     VEHICLE_MODEL_NAME = config["detectors"]["vehicle_detector"]
     LICENSE_MODEL_NAME = config["detectors"]["license_detector"]
-    return { "VEHICLE_MODEL_NAME" : MODEL_ENDPOINT + VEHICLE_MODEL_NAME, "LICENSE_MODEL_NAME": MODEL_ENDPOINT + LICENSE_MODEL_NAME}
+    
+    return ModelInfo(VEHICLE_MODEL_NAME=MODEL_ENDPOINT + VEHICLE_MODEL_NAME, LICENSE_MODEL_NAME=MODEL_ENDPOINT + LICENSE_MODEL_NAME)
