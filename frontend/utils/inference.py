d249fe5d14dacdd79e4135199f8ae5b78280d979
rohgeuntak76 <guen-tak.roh@hpe.com>
2025-06-06 02:23:51 +0900

    becase of server overwhelming, display results at the end

--- frontend/utils/inference.py
+++ frontend/utils/inference.py
@@ -5,17 +5,20 @@ import websocket
 import streamlit as st
 import pandas as pd
 from utils.logging import logger  
+import time
 
-def display_frame(frame_bytes,ann_frame):
+def display_frame(frame_bytes,ann_frame,results_frame):
     logger.info("display frame function called")
     image = io.BytesIO(frame_bytes) # Convert bytes to image
-    ann_frame.image(image) # Update the placeholder
+    results_frame.append(image)
+    # ann_frame.image(image) # Update the placeholder
     logger.info("display frame Done!")
 
-def display_dataframe(detection_results,results_list,result_df):
+# def display_dataframe(detection_results,results_list,result_df):
+def display_dataframe(detection_results,results_list):
     logger.info("display dataframe function called")
     results_json = json.loads(detection_results)
-    result_df.add_rows(results_json)
+    # result_df.add_rows(results_json)
     results_list.extend(results_json) # update list
     logger.info("display dataframe Done!")
 
@@ -45,16 +48,21 @@ def vehicle_detection_video(api_host,sess_id,video_path,selected_classes,vehicle
     else:
         ws_url = f"ws://{api_host}/api/video/ws/vehicle/"
     ws_url = ws_url + sess_id
+    results_frame = []
     ws = websocket.WebSocketApp(
         ws_url,
-        on_message=lambda ws, msg: display_frame(msg,ann_frame),
+        on_message=lambda ws, msg: display_frame(msg,ann_frame,results_frame),
         on_error=lambda ws, err: st.error(f"Error: {err}"),
         on_close=lambda ws,status,msg: st.info(f"Processing complete. code {status} : {msg}")
     )
 
     # Send video path to backend
     ws.on_open = lambda ws: ws.send(json.dumps({"video_path": video_path,"conf":vehicle_conf,"classes":selected_ind,"ratio":video_inference_ratio}))
-    ws.run_forever()
+    with st.spinner("Wait for Inferencing...", show_time=True):
+        ws.run_forever()
+    for frame in results_frame:
+        ann_frame.image(frame)
+        time.sleep(0.02)
 
 def license_number_image_infer(api_host,vid_file,vehicle_conf,license_conf):
     files = {
@@ -85,10 +93,11 @@ def license_number_image_visualize(api_host,vid_file,detection_result):
 
 def license_number_video_infer(api_host,sess_id,video_path,vehicle_conf,license_conf,video_inference_ratio):
     results_list = []
-    result_df = st.dataframe(pd.DataFrame(columns=("frame_number","track_id","vehicle_bbox","vehicle_bbox_score","lp_bbox","lp_bbox_score","lp_number","lp_text_score")),hide_index=True)
+    # result_df = st.dataframe(pd.DataFrame(columns=("frame_number","track_id","vehicle_bbox","vehicle_bbox_score","lp_bbox","lp_bbox_score","lp_number","lp_text_score")),hide_index=True)
     ws = websocket.WebSocketApp(
         f"ws://{api_host}/api/video/ws/license_number/{sess_id}",
-        on_message=lambda ws, msg: display_dataframe(msg,results_list,result_df),
+        # on_message=lambda ws, msg: display_dataframe(msg,results_list,result_df),
+        on_message=lambda ws, msg: display_dataframe(msg,results_list),
         on_error=lambda ws, err: st.error(f"Error: {err}"),
         on_close=lambda ws,status,msg: st.info(f"Processing complete. code {status} : {msg}")
     )
@@ -99,13 +108,19 @@ def license_number_video_infer(api_host,sess_id,video_path,vehicle_conf,license_
 
 
 def license_number_video_visualize(api_host,sess_id,video_path,detection_result,ann_frame):
+    results_frame = []
     ws = websocket.WebSocketApp(
         f"ws://{api_host}/api/utils/ws/draw/annotated/{sess_id}",
-        on_message=lambda ws,msg: display_frame(msg,ann_frame),
+        on_message=lambda ws,msg: display_frame(msg,ann_frame,results_frame),
         on_error=lambda ws,err: st.error(f"Error: {err}"),
         on_close=lambda ws,status,msg: st.info(f"Processing complete. code {status} : {msg}")
     )
 
     # Send video path to backend
     ws.on_open = lambda ws: ws.send(json.dumps({"video_path": video_path,"detection_results":detection_result}))
-    ws.run_forever()
+    with st.spinner("Wait for Visualization...", show_time=True):
+        ws.run_forever()
+
+    for frame in results_frame:
+        ann_frame.image(frame)
+        time.sleep(0.02)
