36ea98d6ed7346182e4c98496cb7683b5e009ae5
rohgeuntak76 <guen-tak.roh@hpe.com>
2025-06-11 18:50:48 +0900

    delete old one

--- finetune/.ipynb_checkpoints/run_finetune_with_mlflow_python-checkpoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-timestamp=$(date +%F_%H-%M-%S)
-epochs=30
-batch=0.6
-
-echo $timestamp $epochs $batch
-
-python finetune_with_mlflow.py --epochs $epochs --batch $batch > finetune_${epochs}_${batch}_${timestamp}.log 2>&1 &
